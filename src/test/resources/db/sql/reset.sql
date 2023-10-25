00114989242c8ab0d58b0d60ec546dd4c609dc82
Glauber de Oliveira Souza <32614699+glauberosouza@users.noreply.github.com>
2023-10-25 14:33:02 -0300

    Update reset.sql

--- src/test/resources/db/sql/reset.sql
+++ src/test/resources/db/sql/reset.sql
@@ -1,19 +1,19 @@
 
--- Desativar temporariamente as verificações de chave estrangeira
+
 SET REFERENTIAL_INTEGRITY FALSE;
 
--- Exclua todos os registros da tabela PURCHASE
+
 DELETE FROM PURCHASE;
 
--- Exclua todos os registros da tabela TICKET
+
 
 DELETE FROM TICKET;
 
--- Reinicie o valor do ID auto-incremento da tabela PURCHASE
+
 ALTER TABLE PURCHASE ALTER COLUMN ID RESTART WITH 1;
 
--- Reinicie o valor do ID auto-incremento da tabela TICKET
+
 ALTER TABLE TICKET ALTER COLUMN ID RESTART WITH 1;
 
--- Reativar as verificações de chave estrangeira
+
 SET REFERENTIAL_INTEGRITY TRUE;
